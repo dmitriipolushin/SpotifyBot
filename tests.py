5b298036f476b5d17e4ed1ef05ee04300337aa99
dmitriipolushin <27102001@bk.ru>
2022-11-21 18:18:07 +0300

    add another test

--- tests.py
+++ tests.py
@@ -1,8 +1,15 @@
 import json
 from main import get_information
+from main import get_iq_articles
 
 
 def test_get_information_type():
     f = open('item_test.json')
     item = json.load(f)
     assert isinstance(get_information(item), dict)
+
+def test_get_iq_articles_error():
+    try:
+        get_iq_articles('test')
+    except:
+        assert True
